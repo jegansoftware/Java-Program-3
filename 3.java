9e20eabe9fa0376cf9a3fb9e3eced3bf65e614e6
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-07-08 04:31:23 +0530

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,23 @@
+//file name should be named as hypotenuse.java
+
+package Java;
+
+public class hypotenuse {
+	
+	hypotenuse()
+	{
+		int a=3,b=4,c=0,c1;
+		System.out.printf("Enter A Value :");
+		
+		
+		System.out.printf("Enter B Value :");
+		
+		c=a*a+b*b;
+		c1=c*c;
+		
+		System.out.printf("\n%d",c1);
+		
+		
+	}
+
+}
