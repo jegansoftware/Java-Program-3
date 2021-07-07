e3ddd7a84e42a85107111032238241471681fd38
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-07-08 04:30:25 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+//file name should be named as cone.java
+
+package Java;
+
+public class cone {
+
+	public static void main(String[] args) {
+	
+		Volume_of_cone in1=new Volume_of_cone();
+		in1.cone();
+		fahrenheit iin1=new fahrenheit();
+		hypotenuse ini1=new hypotenuse();
+		juliandate date=new juliandate();
+		date.main();
+		
+	}
+
+}
